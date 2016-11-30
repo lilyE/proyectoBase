1bf87bf29e447feae337f409a87aac01219dd249
lilyE <ola.soi.liy@gmail.com>
2016-11-29 20:05:14 -0400

    proyecto

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+var reclamo={tipoReclamo:"odeca",observacion:"mal servicio",fechaREclamo:"29/11/2016"}
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'prooyecto final', subtitle:'avances en computacion',reclamo: reclamo});
+
 });
 
 module.exports = router;
